0654a3ddaadab1d8e17067594bca58ac2315b904
mostofameem <mostofameem@gmail.com>
2026-01-03 20:43:40 +0600

    feat: update migrations

--- backend/internal/migrations/000010_create_table_events.up.sql
+++ backend/internal/migrations/000010_create_table_events.up.sql
@@ -1,10 +1,11 @@
 -- +migrate Up
 
 CREATE TABLE IF NOT EXISTS events (
-    id BIGINT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     event_type_id INT NOT NULL,
+    should_auto_create_event BOOLEAN DEFAULT false,
     start_at TIMESTAMP NOT NULL,
     registration_opens_at TIMESTAMP,
     registration_closes_at TIMESTAMP,
@@ -15,3 +16,6 @@ CREATE TABLE IF NOT EXISTS events (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX IF NOT EXISTS idx_events_registration
+ON events (registration_opens_at, registration_closes_at);
